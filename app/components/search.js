f4db93b3f37673c3c51d0436b6c460371bafbf70
Allen Jiang <allenjiang17@gmail.com>
2023-08-06 15:41:14 -0400

    added shuffle button

--- app/components/search.js
+++ app/components/search.js
@@ -43,7 +43,7 @@ export function Search(props){
           <SearchBar handlename = {handleInput}/>  
           <SearchBarResults showList = {showList} />
         </div>
-        <SearchDashboard list_of_tempos = {list_of_tempos} handlename = {handleFilterInput} display = {props.dashboard_display}/>
+        <SearchDashboard showList = {showList} list_of_tempos = {list_of_tempos} handlename = {handleFilterInput} display = {props.dashboard_display}/>
       </div> 
     )
   }
@@ -82,11 +82,27 @@ export function Search(props){
   }
 
   export function SearchDashboard(props) {
+    const {update_current_song, update_current_set_song} = useContext(SongContext); 
 
     const tempo_options = props.list_of_tempos.map((tempo,index) => {return (<option key={index+1}value={tempo}>{tempo}</option>)})
 
+    function shuffle() {
+
+      let select_song = props.showList[Math.floor(Math.random() * props.showList.length)];
+
+      //need to manipulate DOM to find and focus the search element selected
+      let select_song_element = document.querySelector(`[data-id=${select_song.id}]`);
+      select_song_element.scrollIntoView({block: 'center', behavior: "smooth"});
+      select_song_element.focus({preventScroll: true});
+
+      update_current_song(select_song);
+      update_current_set_song(-1);
+
+    }
+  
+    
     return(
-        <div className={`${props.display} text-sm`}>
+        <div className={`${props.display} text-sm flex justify-between mr-1`}>
             <label>Tempo:
                 <select className="box-border min-w-fit text-xs p-2 border-b border-gray-300 focus:outline-gray-300 focus:border-gray-300 dark:bg-gray-800" defaultValue="Any" name="tempo_options" id="tempo_options" 
                     onChange={props.handlename}>
@@ -94,8 +110,13 @@ export function Search(props){
                         {tempo_options}
                 </select>
             </label>
-
+            <button onClick={shuffle}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shuffle" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5z"/>
+                  <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z"/>
+                </svg>
+            </button>
         </div>
     )
   }
-  
+
